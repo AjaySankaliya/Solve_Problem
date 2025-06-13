31824fb6cea48896b8b6b5247139958f622480fd
Ajay Sankaliya <120821791+AjaySankaliya@users.noreply.github.com>
2025-06-13 19:28:52 +0530

    kth_potision.java

--- Array/kth_potision.java
+++ Array/kth_potision.java
@@ -13,53 +13,57 @@
 // The 7th element is 256.
 
 import java.util.Scanner;
-public class kth_potision {
+
+public class kth_position {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
 
-        int arr1[]={100, 112, 256, 349, 770};
+        int arr1[] = { 100, 112, 256, 349, 770 };
 
-        int arr2[]={72, 86, 113, 119, 265, 445, 892};
+        int arr2[] = { 72, 86, 113, 119, 265, 445, 892 };
 
-        int newarr[]=new int[arr1.length+arr2.length];
+        int newArr[] = new int[arr1.length + arr2.length];
 
-        for(int i=0;i<arr1.length;i++)
-        {
-            newarr[i]=arr1[i]; 
+        for (int i = 0; i < arr1.length; i++) {
+            newArr[i] = arr1[i];
         }
 
-        for(int i=0;i<arr2.length;i++)
-        {
-            newarr[arr1.length+i]=arr2[i]; 
+        for (int i = 0; i < arr2.length; i++) {
+            newArr[arr1.length + i] = arr2[i];
         }
 
-       
-        for(int i=0;i<newarr.length;i++)
-        {
-            for(int j=0;j<newarr.length-i-1;j++)
-            {
-                if(newarr[j]>newarr[j+1])
-                {
-                    int temp=newarr[j];
-                    newarr[j]=newarr[j+1];
-                    newarr[j+1]=temp;
+        for (int i = 0; i < newArr.length; i++) {
+            for (int j = 0; j < newArr.length - i - 1; j++) {
+                if (newArr[j] > newArr[j + 1]) {
+                    int temp = newArr[j];
+                    newArr[j] = newArr[j + 1];
+                    newArr[j + 1] = temp;
                 }
             }
         }
 
-        for(int i=0;i<newarr.length;i++)
-        {
-            System.out.println(newarr[i]);
+        for (int i = 0; i < newArr.length; i++) {
+            System.out.println(newArr[i]);
         }
 
         System.out.println("Enter k_th position for find element: ");
-        int k=sc.nextInt();
-        
-        if (k <= 0 || k > newarr.length) {
+        int k = sc.nextInt();
+
+        if (k <= 0 || k > newArr.length) {
             System.out.println("Invalid k value.");
         } else {
-            System.out.println("The element at position " + k + " is: " + newarr[k - 1]);
+            System.out.println("The element at position " + k + " is: " + newArr[k - 1]);
         }
-       
+
+        //middle of array
+        if (newArr.length % 2 == 0) {
+            int mid = newArr[newArr.length / 2 - 1];
+            System.out.println("Middle elements: " + mid);
+        } else {
+            int mid = newArr[newArr.length / 2];
+            System.out.println("Middle element: " + mid);
+        }
+
     }
 }
