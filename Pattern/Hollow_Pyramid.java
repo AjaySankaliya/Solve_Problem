59bd735fa9b9d95099cc31055e778948e0ee1f2c
Ajay Sankaliya <120821791+AjaySankaliya@users.noreply.github.com>
2025-08-04 17:48:23 +0530

    pattern

---
+++ Pattern/Hollow_Pyramid.java
@@ -0,0 +1,25 @@
+public class Hollow_Pyramid {
+    public static void main(String[] args) {
+        int n=5;
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int s=1;s<=n-i;s++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                if(j==1 || j==i || i==n)
+                {
+                    System.out.print(j+" ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
